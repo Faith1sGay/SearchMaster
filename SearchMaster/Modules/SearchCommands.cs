f9ac45897092e5136d5e76c57ae17751c30372ae
Faith1sGay <daniel.viola001@mymdc.net>
2021-06-08 19:10:44 -0400

    new Conversions() new Conversions() new Conversions()

--- SearchMaster/Modules/SearchCommands.cs
+++ SearchMaster/Modules/SearchCommands.cs
@@ -6,12 +6,6 @@ namespace SearchMaster.Modules.Searches
 {
     public class Searches : ModuleBase
     {
-        [Command("Test")]
-        public async Task TestAsync()
-        {
-            await ReplyAsync("test");
-        }
-
         [Command("WeatherSearch")]
         public async Task WeatherSearchAsync(string city)
         {
@@ -22,7 +16,7 @@ namespace SearchMaster.Modules.Searches
                 .AddField(":cloud: Cloud Coverage", e.clouds.all + "%")
                 .AddField(":sweat: Humidity", e.main.humidity + "%")
                 .AddField(":dash: Wind Speed", e.wind.speed + "m/s")
-                .AddField(":thermometer: Low / High", new Conversions().KelvinToFehrenheit(e.main.temp_min) + " ° " + "/" + new Conversions().KelvinToFehrenheit(e.main.temp_max) + " ° ");
+                .AddField(":thermometer: Low / High", new Conversions().KelvinToFehrenheit(e.main.temp_min) + " ° F - " + new Conversions().KelvinToFehrenheit(e.main.temp_max) + " ° F\n" + new Conversions().KelvinToCelcius(e.main.temp_min) + "° C - " + new Conversions().KelvinToCelcius(e.main.temp_max) + "° C");
             await Context.Channel.SendMessageAsync(embed: embed.Build());
             if (e.cod == 404)
             {
@@ -31,5 +25,22 @@ namespace SearchMaster.Modules.Searches
                 await Context.Channel.SendMessageAsync(embed: Eembed.Build());
             }
         }
+        [Command("say")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task SayAsync([Remainder] string message)
+        {
+            var sayEmbed = new EmbedBuilder()
+            .WithDescription(message);
+            await Context.Channel.SendMessageAsync(embed: sayEmbed.Build());
+        }
+        [Command("bible")]
+        public async Task BibleAsync(string book, string chapterAndVerse)
+        {
+            var bibleLookup = await new BibleApiSearch().BibleApiAsync(book, chapterAndVerse);
+            var embed = new EmbedBuilder()
+                .WithTitle($"{bibleLookup.text}");
+            await Context.Channel.SendMessageAsync(embed: embed.Build());
+        }
+
     }
 }
---
+++ SearchMaster/Services/BibleApi.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace SearchMaster
+{
+    public class Vers
+    {
+        public string book_id { get; set; }
+        public string book_name { get; set; }
+        public int chapter { get; set; }
+        public int verse { get; set; }
+        public string text { get; set; }
+    }
+
+    public class BibleApi
+    {
+        public string reference { get; set; }
+        public List<Vers> verses { get; set; }
+        public string text { get; set; }
+        public string translation_id { get; set; }
+        public string translation_name { get; set; }
+        public string translation_note { get; set; }
+    }
+    public class BibleApiSearch
+    {
+        public async Task<BibleApi> BibleApiAsync(string book, string chapterAndVerse)
+        {
+            HttpClient client = new HttpClient();
+            var getUrl = await client.GetAsync("https://bible-api.com/" + book + " " + chapterAndVerse);
+            var deserialize = JsonSerializer.Deserialize<BibleApi>(await getUrl.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            return deserialize;
+        }
+    }
+}
